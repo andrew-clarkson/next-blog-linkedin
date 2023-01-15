fcbf4c3c2a511587d73cdf524263bd25db4e4837
Andrew Clarkson <94328514+andrew-clarkson@users.noreply.github.com>
2023-01-15 14:58:51 -0500

    make welcome card 2 columns

--- frontend/components/articles.js
+++ frontend/components/articles.js
@@ -9,44 +9,59 @@ const Articles = ({ articles }) => {
 
   const welcome = (
     <div>
-      <p className="uk-text-bold">
-        Welcome! I will be using this space to repost my writing from LinkedIn
-        in a more accessible and eventually searchable/filterable place. This
-        will make advice easier to find, and I will also be expanding on certain
-        projects (such as my #buildinpublic project starting January).
-      </p>
-      <p className="uk-text-bold">You can also find me here:</p>
-      <p>
-        Daily Junior Developer Advice on{" "}
-        <a href="https://www.linkedin.com/in/andrewtclarkson/"> My LinkedIn</a>
-      </p>
-      <p>
-        Listen to the{" "}
-        <a href="https://linktr.ee/loonieengineering">Loonie Engineering</a>{" "}
-        podcast
-      </p>
-      <p>
-        Join{" "}
-        <a href="https://list-manage.us14.list-manage.com/subscribe?u=52487365461b8c86592dec0b2&id=5941a0fcc9">
-          {"Andrew's Coffee Chats"}
-        </a>{" "}
-        (every other Thursday @7pm)
-      </p>
-      <p>
-        Work with me to land the job quicker{" "}
-        <a href="https://introwise.com/andrew-clarkson">
-          Tech Job Seeker Coaching
-        </a>
-      </p>
+      <div className="uk-child-width-1-2@s" data-uk-grid="true">
+        <div>
+          <p className="uk-text-bold">
+            Welcome! I will be using this space to repost my writing from
+            LinkedIn in a more accessible and eventually searchable/filterable
+            place. This will make advice easier to find, and I will also be
+            expanding on certain projects (such as my #buildinpublic project
+            starting January).
+          </p>
+        </div>
+        <div>
+          <p className="uk-text-bold uk-margin-remove-bottom">
+            You can also find me here:
+          </p>
+          <ul className="uk-list-disc uk-margin-remove-top">
+            <li>
+              {"Daily Junior Developer Advice on "}
+              <a href="https://www.linkedin.com/in/andrewtclarkson/">
+                My LinkedIn
+              </a>
+            </li>
+            <li>
+              {"Listen to the "}
+              <a href="https://linktr.ee/loonieengineering">
+                {"Loonie Engineering "}
+              </a>
+              podcast
+            </li>
+            <li>
+              {"Join "}
+              <a href="https://list-manage.us14.list-manage.com/subscribe?u=52487365461b8c86592dec0b2&id=5941a0fcc9">
+                {"Andrew's Coffee Chats"}
+              </a>
+              {" (every other Thursday @7pm)"}
+            </li>
+            <li>
+              {"Work with me to land the job quicker "}
+              <a href="https://introwise.com/andrew-clarkson">
+                Tech Job Seeker Coaching
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <hr />
     </div>
   )
 
   return (
     <div>
+      {welcome}
       <div className="uk-child-width-1-2@s" data-uk-grid="true">
         <div>
-          {welcome}
           {leftArticles.map((article, i) => {
             return (
               <Card
